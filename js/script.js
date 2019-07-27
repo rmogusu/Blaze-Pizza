43ae40e02b31adb3ba9c251dbcc0a5b438029880
mrsfreddy <rmogusu123@gmal.com>
2019-07-27 13:19:21 -0400

    Style my pizza images

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
-// $(document).ready(function() {
-//   $("#add-pizza").click(function() {
-//     $("#new-addresses").append('<div class="new-address">' +
-//                                  '<div class="form-group">' +
-//                                    '<label for="new-street">Street</label>' +
-//                                    '<input type="text" class="form-control new-street">' +
-//                                  '</div>' +
-//                                  '<div class="form-group">' +
-//                                    '<label for="new-city">City</label>' +
-//                                    '<input type="text" class="form-control new-city">' +
-//                                  '</div>' +
-//                                  '<div class="form-group">' +
-//                                    '<label for="new-county">County</label>' +
-//                                    '<input type="text" class="form-control new-county">' +
-//                                  '</div>' +
-//                                '</div>');
-//   });
+$(document).ready(function() {
+  $("#add-pizza").click(function() {
+    $("#new-addresses").append('<div class="new-address">' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-street">Street</label>' +
+                                   '<input type="text" class="form-control new-street">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-city">City</label>' +
+                                   '<input type="text" class="form-control new-city">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-county">County</label>' +
+                                   '<input type="text" class="form-control new-county">' +
+                                 '</div>' +
+                               '</div>');
+  });
