8ec1a484779fd5d6fc6ae87e3e30f2f8522bfca6
mrsfreddy <rmogusu123@gmal.com>
2019-07-28 10:24:13 -0400

    Add h3 and style my h3

--- js/script.js
+++ js/script.js
@@ -1,17 +1,74 @@
-$(document).ready(function() {
-  $("#add-pizza").click(function() {
-    $("#new-addresses").append('<div class="new-address">' +
-                                 '<div class="form-group">' +
-                                   '<label for="new-street">Street</label>' +
-                                   '<input type="text" class="form-control new-street">' +
-                                 '</div>' +
-                                 '<div class="form-group">' +
-                                   '<label for="new-city">City</label>' +
-                                   '<input type="text" class="form-control new-city">' +
-                                 '</div>' +
-                                 '<div class="form-group">' +
-                                   '<label for="new-county">County</label>' +
-                                   '<input type="text" class="form-control new-county">' +
-                                 '</div>' +
-                               '</div>');
-  });
+
+const bacon ={
+  name: "Bacon",
+  price: 150
+};
+const pepporoni = {
+  name: "Bell Pepper",
+  price: 100
+};
+const sausage= {
+  name: "sausage",
+  price: 150
+};
+
+const extraCheese = {
+  name: "extraCheese",
+  price: 70
+};
+const black olive = {
+  name: "black olive",
+  price: 100
+};
+const mushrooms = {
+  name: "Mushrooms",
+  price: 100
+};
+const toppingsList = {
+  name: "Toppings",
+  items: [sausage, bacon, extraCheese,black olive , pepporoni, mushrooms]
+}
+const large = {
+  name: "Large Size",
+  price:1000
+};
+const medium = {
+  name: "Medium Size",
+  price: 800
+};
+const small = {
+  name: "Small Size",
+  price: 700
+};
+const sizeList = {
+  name: "Sizes",
+  items: [large, medium, small]
+};
+const thin = {
+  name: "Thin Crust",
+  price: 70
+};
+const thick = {
+  name: "Thick Crust",
+  price: 100
+};
+const deep = {
+  name: "Deep Crust",
+  price: 100
+};
+const glutten = {
+  name: "Cheese Crust",
+  price: 150
+};
+const stuffed = {
+  name: "Stuffed Crust",
+  price: 250
+};
+const cripsy = {
+  name: "Stuffed Crust",
+  price: 250
+}
+const crustsList = {
+  name: "Crusts",
+  items: [thin, thick, deep, cripsy,glutten, stuffed]
+};
